8d7376eabaaa324f0ef29d36f959844204af03d1
Shreesh-code <83659368+Shreesh-code@users.noreply.github.com>
2021-09-20 17:01:37 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,28 @@
+canvas = document.getElementById("canvs");
+ctx = canvas.getContext("2d");
+
+
+color="blue";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200, 200, 50, 0, 2*Math.PI );
+ctx.stroke();
+
+canvas.addEventListener("mousedown",moosedown);
+function moosedown(e) {
+    mousex = e.clientX - canvas.offsetLeft;
+    mousey = e.clientY - canvas.offsetTop;
+    console.log("X= " + mousex + " Y= " + mousey);
+    circle(mousex, mousey);
+
+}
+
+function circle(mousex, mousey) {
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(mousex, mousey, 40, 0, 2*Math.PI );
+ctx.stroke();
+}
